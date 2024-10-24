67f33691153b3526e537e07ebe5dce9709b578c7
Ilan <ilan961128@gmail.com>
2024-10-25 00:40:35 +0300

    CORS

--- server.js
+++ server.js
@@ -4,6 +4,27 @@ var bodyParser = require("body-parser");
 const app = express();
 const mongoose = require("mongoose");
 
+
+// ---------- CORS ----------
+const cors = require("cors");
+const corsOptions = {
+    origin: "*",
+    credentials: true,
+    optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+
+// ---------- Body Parser ----------
+app.use(express.json());
+app.use(express.static("public"));
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+);
+app.use(bodyParser.json());
+
+
 // ---------- Connect to DB ----------
 const mongoUserName = process.env.MONGO_USER_NAME;
 const mongoPassword = process.env.MONGO_PASSWORD;
@@ -16,7 +37,7 @@ mongoose
     )
     .then(() => {
         app.listen(port, () => {
-            console.log(`DB is Connected on port ${port}!`);
+            console.log(`DB Connected! port ${port}!`);
         });
     })
     .catch((error) => {
