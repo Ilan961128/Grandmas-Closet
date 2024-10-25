81b878ddf33fbc4249fdcabb89cb959b53e19403
Ilan <ilan961128@gmail.com>
2024-10-25 17:58:55 +0300

    Enter Sesstion

--- server.js
+++ server.js
@@ -2,9 +2,42 @@ require('dotenv').config();
 const express = require("express");
 var bodyParser = require("body-parser");
 const app = express();
+const crypto = require("crypto");
 const mongoose = require("mongoose");
 const Users = require("./models/users");
+var session = require("express-session");
 
+// ---------- Session ----------
+session.user_id = null;
+session.admin = null;
+
+const checkSession = (user_type = 0) => {
+    if (user_type == "admin") {
+        if (session.admin === null) {
+            return false;
+        }
+    }
+    else
+        if (session.user_id === null) {
+            return false;
+        }
+
+    return true;
+};
+
+const generateRandomString = () => {
+    return crypto.randomBytes(32).toString("hex");
+};
+
+app.use(
+    session({
+        secret: generateRandomString(),
+        resave: false,
+        saveUninitialized: true,
+        user_id: null,
+        admin: null
+    })
+);
 
 
 // ---------- CORS ----------
@@ -48,6 +81,31 @@ app.post("/user", async (req, res) => {
         });
 
         return res.redirect("http://127.0.0.1:5500/signin.html");
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: error.message });
+    }
+});
+
+// ---------- Create SessionID with UserID ----------
+app.get("/SetSessionID/:user_id", async (req, res) => {
+    try {
+        const the_user_id = req.params.user_id;
+
+        if (!the_user_id) {
+            return res.status(404).json({ message: "Invalid Session ID" });
+        }
+        session.user_id = the_user_id;
+        const user = await Users.findById(the_user_id);
+        const admin = user.admin;
+
+        if (admin === "master") {
+            session.admin = "master";
+            console.log("Admin User");
+        }
+
+        createLog("Signin", "", req);
+        res.status(200).json({ the_user_id });
     } catch (error) {
         console.log(error);
         res.status(500).json({ message: error.message });
